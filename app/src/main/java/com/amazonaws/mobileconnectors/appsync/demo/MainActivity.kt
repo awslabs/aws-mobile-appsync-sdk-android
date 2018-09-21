8ca3049c2ec2e348f3bff2b6315a645453212bc5
cbommas <chabom@amazon.com>
2018-09-21 11:33:00 -0700

    Added graphql files

--- app/src/main/java/com/amazonaws/mobileconnectors/appsync/demo/MainActivity.kt
+++ app/src/main/java/com/amazonaws/mobileconnectors/appsync/demo/MainActivity.kt
@@ -1,24 +1,13 @@
-package com.example.appsyncsample
+package com.amazonaws.mobileconnectors.appsync.demo
 
-import android.content.Intent
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.button_single).setOnClickListener {
-            val intent = Intent(this, SingleActivity::class.java)
-            startActivity(intent)
-        }
-
-        findViewById<Button>(R.id.button_multi).setOnClickListener {
-            val intent = Intent(this, MultiActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
