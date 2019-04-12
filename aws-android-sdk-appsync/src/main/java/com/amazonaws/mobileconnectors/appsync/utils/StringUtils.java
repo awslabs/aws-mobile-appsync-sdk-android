bb12482ab3c8c20b569bf4ff0c2a3ab634e6e092
alanMTL11 <39388837+alanvan0502@users.noreply.github.com>
2019-04-12 22:16:49 +0700

    Fix NPE bug in ApolloServerInterceptor; sanitize useragent string (#146)
    The user agent string contained unicode, specifically (TM) symbol obtained from the platform name.

---
+++ aws-android-sdk-appsync/src/main/java/com/amazonaws/mobileconnectors/appsync/utils/StringUtils.java
@@ -0,0 +1,23 @@
+package com.amazonaws.mobileconnectors.appsync.utils;
+
+import okio.Buffer;
+
+public class StringUtils {
+    public static String toHumanReadableAscii(String s) {
+        for (int i = 0, length = s.length(), c; i < length; i += Character.charCount(c)) {
+            c = s.codePointAt(i);
+            if (c > '\u001f' && c < '\u007f') continue;
+
+            Buffer buffer = new Buffer();
+            buffer.writeUtf8(s, 0, i);
+            for (int j = i; j < length; j += Character.charCount(c)) {
+                c = s.codePointAt(j);
+                if (c > '\u001f' && c < '\u007f') {
+                    buffer.writeUtf8CodePoint(c);
+                }
+            }
+            return buffer.readUtf8();
+        }
+        return s;
+    }
+}
